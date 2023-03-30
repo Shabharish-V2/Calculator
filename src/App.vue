9e3f5a33a910bcdc41de8de5d8975297d3fe0c5e
Shabharish <111118584+Shabharish-V2@users.noreply.github.com>
2023-03-30 17:55:47 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyCalculator />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import MyCalculator from './components/MyCalculator.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
---
+++ src/components/MyCalculator.vue
@@ -0,0 +1,71 @@
+<template>
+    <div id="calc">
+        <div class="screen">{{ screen }}</div>
+
+
+        <div @click="concatNum('1')" class="btn num">1</div>
+        <div @click="concatNum('2')" class="btn num">2</div>
+
+
+        <div @click="concatNum('3')" class="btn num">3</div>
+        <div @click="concatNum('4')" class="btn num">4</div>
+
+
+        <div @click="concatNum('5')" class="btn num">5</div>
+        <div @click="concatNum('6')" class="btn num">6</div>
+
+        <div @click="concatNum('7')" class="btn num">7</div>
+        <div @click="concatNum('8')" class="btn num">8</div>
+
+
+        <div @click="concatNum('9')" class="btn num">9</div>
+
+        <div @click="concatNum('0')" class="btn num">0</div>
+        <div @click="concatNum('.')" class="btn num">.</div>
+
+        <div @click="evaluate()" class='btn equal'>=</div>
+        <div @click="concatOP('*')" class="btn num">*</div>
+        <div @click="concatOP('%')" class="btn num">%</div>
+        <div @click="concatOP('/')" class="btn num">/</div>
+        <div @click="concatOP('-')" class="btn num">-</div>
+        <div @click="concatOP('+')" class="btn num">+</div>
+        <div @click="clear()" class='btn clear'>C</div>
+    </div>
+</template>
+
+
+<script setup>
+import { ref } from 'vue';
+let screen = ref(0);
+const concatNum=(num)=>{
+   screen.value === 0? (screen.value=num) : (screen.value += num);
+}
+const concatOP=(OP)=>{
+    if(screen.value === 0 || screen.value === "."){
+        if(OP == '-'){
+            screen.value = OP;
+            return;
+        } else {
+            return;
+        }
+    }
+    screen.value += OP;
+}
+const clear=()=>{
+    screen.value=0;
+}
+const evaluate=()=>{
+     screen.value = eval(screen.value);
+}
+
+</script>
+
+
+  
+
+
+
+
+<style scoped lang="scss">
+@import "@/components/styles/MyCalculator.scss";
+</style>
